298c047003998b14138327adfcad92980b49c813
Biruk G <biruk2121@gmail.com>
2023-10-29 12:05:43 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+
+import './commonResource/css/styles.css';
+import './commonResource/css/bootstrap.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Alert from './Components/Main/Alert';
+import First from './Components/Main/First';
+import Second from './Components/Main/Second';
+import Third from './Components/Main/Third';
+import Fourth from './Components/Main/Fourth';
+import Fifth from './Components/Main/Fifth';
+import Sixth from './Components/Main/Sixth';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div> 
+      <Header />
+      <Alert />
+      <First />
+      <Second />
+      <Third />
+      <Fourth />
+      <Fifth />
+      <Sixth />
+      <Footer />
+
     </div>
   );
 }
